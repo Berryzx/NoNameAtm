2aae4a6fa59c79c9155afcd6b3d236957d5cf3bc
l3ilko Jr <50190264+l3ilkojr@users.noreply.github.com>
2020-01-21 17:49:48 -0500

    Create Ping.ts

---
+++ commands/Public/Ping.ts
@@ -0,0 +1,19 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+
+export default class PingCommand extends Command {
+    public constructor() {
+        super("ping", {
+            aliases: ["ping"],
+            description: {
+                content: "check the ping of the latency to the API...",
+                examples: ["ping"],
+                usages: "ping"
+            },
+            ratelimit: 3
+        });
+    }
+    public exec(message: Message) {
+        return message.util?.reply(`The ping of the latency to the API is: \`${this.client.ws.ping}ms\`!`);
+    }
+}
