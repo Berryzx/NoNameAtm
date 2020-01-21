1ecc3056df06560b452440f1fe7f96d196a14ef6
l3ilko Jr <50190264+l3ilkojr@users.noreply.github.com>
2020-01-21 17:38:45 -0500

    Created Config.ts
    This is to hold the owner IDS token for the main file plus db name and prefix

---
+++ Config.ts
@@ -0,0 +1,4 @@
+export let token: string = process.env.TOKEN;
+export let owners: string[] = ["661164977675042817", "366026189166346242"];
+export let defaultPrefix: string = "!";
+export let databaseName: string = "BotDB";
