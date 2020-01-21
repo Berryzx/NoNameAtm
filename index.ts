6cb5871e1bcaef14ce4e773df21a72d599fa8819
Berryzx <joshw9907@Gmail.com>
2020-01-21 14:19:32 -0800

    Second commmit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import {loadCommands, handelCommand} from "../Stwb_Bot/src/Api_Config/cmdHandler";
+import {Client} from "discord.js"
+import * as Discord from "discord.js"
+import * as dotenv from "dotenv"
+dotenv.config();
+
+const clinet: Discord.Client = new Client()
+
+loadCommands(`${__dirname}/commands/Moderation`)
+
+
+
+
+clinet.on("message", (msg) => {
+    if (msg.author.bot) { return; }
+
+    if (msg.channel.type == "dm") { return; }
+
+    if (!msg.content.startsWith("!" || "?")) { return; }
+    handelCommand(msg)
+})
+
+clinet.login(process.env.TOKEN)
